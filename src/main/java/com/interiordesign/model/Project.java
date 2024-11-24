f3626a444e0db7bedcd5e13ff6bbcb56b48937fe
Ricardo Pasini <ricardo.pasini@gmail.com>
2024-11-24 18:56:04 +0000

    Add dateCreated to Project

--- src/main/java/com/interiordesign/model/Project.java
+++ src/main/java/com/interiordesign/model/Project.java
@@ -1,5 +1,7 @@
 package com.interiordesign.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,31 +15,46 @@ public class Project {
     private String title;
     private String description;
     private String imageUrl;
+    private LocalDate dateCreated;
 
-    public Long getId() {
+    // Getters and setters
+	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
 	public String getImageUrl() {
 		return imageUrl;
 	}
+	
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
 	
-    // Getters and setters
+	public LocalDate getDateCreated() {
+		return dateCreated;
+	}
+	
+	public void setDateCreated(LocalDate dateCreated) {
+		this.dateCreated = dateCreated;
+	}
 }
